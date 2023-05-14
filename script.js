ce4eddf348872bdd956a7fe62354baacf491bd00
jesse <web.jesse@gmail.com>
2023-05-14 23:41:38 +0800

    no work

--- script.js
+++ script.js
@@ -199,6 +199,24 @@ window.onload = function () {
     options: options,
   });
 
+  // download function  
+    document.getElementById("download").addEventListener("click", function () {
+    /*Get image of canvas element*/
+    var url_base64jp = document
+        .getElementById("myChart")
+        .toDataURL("image/jpg");
+    /*get download button (tag: <a></a>) */
+    var a = document.getElementById("download");
+        
+    /*insert chart image url to download button (tag: <a></a>) */
+    a.href = url_base64jp;
+    });
+    
+
+        
+
+
+
   // click handler for chart-tab.
   document.getElementById("chart-tab").addEventListener("click", function () {
     // get myChart data[0] from getIncomeValues
