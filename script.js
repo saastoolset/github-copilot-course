588eb6b9fd48751ff226afee9b965b594cdec1b5
jesse <web.jesse@gmail.com>
2023-05-14 22:40:30 +0800

    add dataset

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 // on window load
 window.onload = function() {
+    // get input value for janIncome.
+    var janIncome = document.getElementById('janIncome').value;
+    // get input value for febIncome.
+    var febIncome = document.getElementById('febIncome').value;
+    // get input value for marIncome.
+    var marIncome = document.getElementById('marIncome').value;
+    // get input value for aprIncome.
+    var aprIncome = document.getElementById('aprIncome').value;
+    // get input value for mayIncome.
+    var mayIncome = document.getElementById('mayIncome').value;
+    // get input value for junIncome.
+    var junIncome = document.getElementById('junIncome').value;
+    // get input value for julIncome.
+    var julIncome = document.getElementById('julIncome').value;
+    // get input value for augIncome.
+    var augIncome = document.getElementById('augIncome').value;
+    // get input value for sepIncome.
+    var sepIncome = document.getElementById('sepIncome').value;
+    // get input value for octIncome.
+    var octIncome = document.getElementById('octIncome').value;
+    // get input value for novIncome.
+    var novIncome = document.getElementById('novIncome').value;
+    // get input value for decIncome.
+    var decIncome = document.getElementById('decIncome').value;
+    // get input value for janExpense.
+    var janExpense = document.getElementById('janExpenses').value;
+    // get input value for febExpense.
+    var febExpense = document.getElementById('febExpenses').value;
+    // get input value for marExpense.
+    var marExpense = document.getElementById('marExpenses').value;
+    // get input value for aprExpense.
+    var aprExpense = document.getElementById('aprExpenses').value;
+    // get input value for mayExpense.
+    var mayExpense = document.getElementById('mayExpenses').value;
+    // get input value for junExpense.
+    var junExpense = document.getElementById('junExpenses').value;
+    // get input value for julExpense.
+    var julExpense = document.getElementById('julExpenses').value;
+    // get input value for augExpense.
+    var augExpense = document.getElementById('augExpenses').value;
+    // get input value for sepExpense.
+    var sepExpense = document.getElementById('sepExpenses').value;
+    // get input value for octExpense.
+    var octExpense = document.getElementById('octExpenses').value;
+    // get input value for novExpense.
+    var novExpense = document.getElementById('novExpenses').value;
+    // get input value for decExpense.
+    var decExpense = document.getElementById('decExpenses').value;
+
+
+
     // create Chart.js bar chart for ID myChart
     var ctx = document.getElementById('myChart').getContext('2d');
     // set Chart.js options
@@ -15,39 +66,89 @@ window.onload = function() {
             }]
         }
     };
+    // 'rgba(255, 0, 0, 0.5)',
+    // 'rgba(0, 0, 255, 0.5)',
+    // 'rgba(255, 0, 0, 0.8)',
+    // 'rgba(0, 0, 255, 0.8)',
+
+
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Cyan', 'Magenta', 'Lime', 'Pink'],
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             datasets: [{
-                label: 'Color Intensity',
-                data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+                label: 'Income',
+                data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
                 backgroundColor: [
                     'rgba(255, 0, 0, 0.5)',
-                    'rgba(0, 0, 255, 0.5)',
-                    'rgba(255, 255, 0, 0.5)',
-                    'rgba(0, 255, 0, 0.5)',
-                    'rgba(128, 0, 128, 0.5)',
-                    'rgba(255, 165, 0, 0.5)',
-                    'rgba(0, 255, 255, 0.5)',
-                    'rgba(255, 0, 255, 0.5)',
-                    'rgba(0, 255, 0, 0.5)',
-                    'rgba(255, 192, 203, 0.5)'
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)'
                 ],
                 borderColor: [
                     'rgba(255, 0, 0, 0.8)',
-                    'rgba(0, 0, 255, 0.8)',
-                    'rgba(255, 255, 0, 0.8)',
-                    'rgba(0, 255, 0, 0.8)',
-                    'rgba(128, 0, 128, 0.8)',
-                    'rgba(255, 165, 0, 0.8)',
-                    'rgba(0, 255, 255, 0.8)',
-                    'rgba(255, 0, 255, 0.8)',
-                    'rgba(0, 255, 0, 0.8)',
-                    'rgba(255, 192, 203, 0.8)'
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
+                    'rgba(255, 0, 0, 0.8)',
                 ],
                 borderWidth: 1
-            }]
+            },
+            {
+                label: 'Expense',
+                data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+                backgroundColor: [
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)',
+                    'rgba(0, 0, 255, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                    'rgba(0, 0, 255, 0.8)',
+                ],                
+                borderWidth: 1
+            }
+        ]
         },
         options: options
     });
